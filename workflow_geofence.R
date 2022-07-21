6bf711a3109e231139926041ffafb8ed9236fd14
Chrissy h Roberts <31275801+chrissyhroberts@users.noreply.github.com>
2022-07-21 10:58:50 +0100

    adds more

--- workflow_geofence.R
+++ workflow_geofence.R
@@ -1,25 +1,17 @@
-library(magrittr)
+library(tidyverse)
 library(sf)
 
 
 # Shapefile from ABS:
 # https://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/1270.0.55.004July%202016?OpenDocument
 map = read_sf("SUA_2016_AUST.shp/SUA_2016_AUST.shp")
-
 map <- filter(map,str_detect(map$SUA_NAME16,"Not in any Significant Urban Area")==FALSE)
 
-
-
-
-
 # Now create a grid of all global points at 0.1 degree resolution
+global.0.1 <- expand_grid(lon = seq(-40.0,-10,0.01), lat = seq(110,150.0,0.01))
 
-global.0.1 <- expand_grid(lat = seq(-40.0,-10,0.01), lon = seq(110,150.0,0.01))
-
-
-# Convert the points to an sf object
-
-pnts_sf <- st_as_sf(global.0.1, coords = c('lon', 'lat'), crs = st_crs(map))
+# Convert the points of the matrix to coordinates compatible with sf objects
+pnts_sf <- st_as_sf(global.0.1, coords = c('lat', 'lon'), crs = st_crs(map))
 
 # Find the intersections of the points and the template map
 pnts.intersection <- pnts_sf %>% mutate(
@@ -29,12 +21,23 @@ pnts.intersection <- pnts_sf %>% mutate(
 
 
 # Find only points that fall within the polygons
-inside.polygons<-filter(pnts.intersection,area!="")
-inside.polygons<-inside.polygons %>% mutate(
-  geometry=str_sub(string = geometry,start = 3,end = str_length(geometry)-1)
+inside.polygons<-tibble(filter(pnts.intersection,area!="")) %>%
+  mutate(
+    geometry = as.character(geometry),
+    geometry=str_sub(string = geometry,start = 3,end = str_length(geometry)-1),
+    area = as.factor(area)
+  ) %>%
+  separate(geometry, c("lat", "lon"), ", ")
+
+
 )
 
 
-out<-tibble(inside.polygons)
+%>%
+  mutate(
+)
+
+
+out<-
 
 
